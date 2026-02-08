f579082f811b2d0e49b4bdf9b6ee4cc076cd4013
lidejia <3368720939@qq.com>
2026-02-08 17:13:28 +0800

    feat:新增页面

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+
+
+const Home:React.FC=()=>{
+  return (
+    <>
+    Home
+    </>
+  )
+}
+export default Home;
---
+++ frontend/src/pages/Mine.tsx
@@ -0,0 +1,10 @@
+
+
+const Mine:React.FC=()=>{
+  return (
+    <>
+     Mine
+    </>
+  )
+}
+export default Mine;
