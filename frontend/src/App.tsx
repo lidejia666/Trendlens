37af1e45e0a5a9f026ea9a714d4f7f8bd929d884
lidejia <3368720939@qq.com>
2026-02-08 17:14:33 +0800

    chore:配置路由依赖等

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 const App:React.FC=()=>{
   return (
     <>
-      <h1>Trendlens平台</h1>
+      
     </>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import RouterConfig from '@/routers'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+
+    <RouterConfig>
     <App />
-  </StrictMode>,
+    </RouterConfig>
+
 )
