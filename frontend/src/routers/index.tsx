c2653060aa7e0eed9adf06a36d19186a78991233
lidejia <3368720939@qq.com>
2026-02-11 15:46:58 +0800

    feat:提供全局loading页面

--- frontend/src/routers/index.tsx
+++ frontend/src/routers/index.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import {Suspense,lazy} from 'react'  //路由的懒加载
+import Loading from '@/component/loading';
 
 const Home=lazy(()=>import('@/pages/Home')); 
 const Community=lazy(()=>import('@/pages/Community'));
-const Chatbot=lazy(()=>import('@/pages/chatbot'));
+const Chatbot=lazy(()=>import('@/pages/Chatbot'));
 const Mine=lazy(()=>import('@/pages/Mine'));
 export default function RouterConfig({children}:{children?:React.ReactNode}) {
   return (
     <Router>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<Loading />}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/communtiy" element={<Community />} />
